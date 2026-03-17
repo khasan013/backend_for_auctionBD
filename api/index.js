219ac1a4a85ff9843195edb516712b0564c61b8c
khalid013 <khalidhasan837@gmail.com>
2026-03-17 17:06:56 +0600

    fix: vercel backend crash

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import app from "../app.js";
 import serverless from "serverless-http";
 
-export default serverless(app);
+const handler = serverless(app);
+
+export default async (req, res) => {
+  try {
+    return await handler(req, res);
+  } catch (err) {
+    console.error("🔥 FULL ERROR:", err);
+
+    return res.status(500).json({
+      message: "Server crashed",
+      error: err.message,
+      stack: err.stack,
+    });
+  }
+};
